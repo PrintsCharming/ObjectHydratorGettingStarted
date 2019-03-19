a2dc3a23c49e494a34bf3a3d4ec314a57561b001
Ryan Smith <ryans@ocgoodwill.org>
2019-03-18 22:04:12 -0700

    Demo

---
+++ ObjectHydratorSimpleConsole/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectHydratorSimpleConsole
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+    }
+}
--- ObjectHydratorSimpleConsole/Program.cs
+++ ObjectHydratorSimpleConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using FoundationStandard.ObjectHydrator;
 
 namespace ObjectHydratorSimpleConsole
 {
@@ -6,7 +8,20 @@ namespace ObjectHydratorSimpleConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var customers = new Hydrator<Customer>()
+                .WithInteger(x=>x.Age,0,25)
+                .WithUnitedKingdomLandline(x=>x.PhoneNumber)
+                .GetList(5);
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("{0} {1} is {2} years old and can be phoned at {3}",
+                    customer.FirstName,
+                    customer.LastName,
+                    customer.Age,
+                    customer.PhoneNumber);
+            }
+
+            Console.ReadLine();
         }
     }
 }
