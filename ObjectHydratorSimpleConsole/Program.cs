00b7d6adbeaa2fb142789f3d32c1bab48d17b48d
Ryan Smith <ryans@ocgoodwill.org>
2019-03-18 21:19:48 -0700

    Add project files.

---
+++ ObjectHydratorSimpleConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectHydratorSimpleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
